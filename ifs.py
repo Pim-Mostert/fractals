fc52ec8a689819bf4feb38ba36595574cdabed49
Pim Mostert <mostert.pim@gmail.com>
2024-06-10 21:27:33 +0200

    Added BarnsleyFern

--- ifs.py
+++ ifs.py
@@ -1,16 +1,16 @@
 import abc
 import math
-import random
 import numpy as np
 from typing import List
 
 class IfsFractal(abc.ABC):
-    def __init__(self, x0: np.array, transformations: List[np.array]):
+    def __init__(self, x0: np.array, transformations: List[np.array], p=None):
         self.x = np.append(x0, np.array([1]))
         self.transformations = transformations
+        self.p = p if p is not None else np.ones((len(transformations)))/len(transformations)
         
     def get_next_point(self):
-        next_transformation = random.randint(0, len(self.transformations)-1)
+        next_transformation = np.random.choice(range(0, len(self.transformations)), p=self.p)
         
         self.x = self.x @ self.transformations[next_transformation]
         
@@ -25,5 +25,28 @@ class SierpinskiTriangle(IfsFractal):
         t3 = np.array([[0.5, 0, 0], [0, 0.5, 0], [0.25, math.tan(math.pi/3)*0.25, 1]])
         
         transformations = [t1, t2, t3]
-        
+                
         super().__init__(x0, transformations)
+        
+class BarnsleyFern(IfsFractal):
+    def __init__(self):
+        x0 = np.array([0, 0])
+        
+        t1 = np.array([[0, 0, 0], 
+                       [0, 0.16, 0], 
+                       [0, 0, 1]])
+        t2 = np.array([[0.85, -0.04, 0], 
+                       [0.04, 0.85, 0], 
+                       [0, 1.6, 1]])
+        t3 = np.array([[0.2, 0.23, 0], 
+                       [-0.26, 0.2, 0], 
+                       [0, 1.6, 1]])
+        t4 = np.array([[-0.15, 0.26, 0], 
+                       [0.28, 0.24, 0], 
+                       [0, 0.44, 1]])
+        
+        transformations = [t1, t2, t3, t4]
+        
+        p = [0.01, 0.85, 0.07, 0.07]
+
+        super().__init__(x0, transformations, p)
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import matplotlib.pyplot as plt; plt.ion()
 import numpy as np
-from ifs import SierpinskiTriangle
+
+from ifs import BarnsleyFern, SierpinskiTriangle
+from importlib import reload; reload(BarnsleyFern)
 
 fig, ax = plt.subplots(figsize=(6, 6))
-ax.set_xlim(0, 1)
-ax.set_ylim(0, 1)
 
 # Draw fractal
-n_points = 10000
+n_points = 100000
 
 x = np.zeros((2, n_points))
 
-sierpinski = SierpinskiTriangle()
+fractal = BarnsleyFern()
 
 for i in range(0, n_points):
-    x[:, i] = sierpinski.get_next_point()
+    x[:, i] = fractal.get_next_point()
 
-ax.scatter(x[0, :], x[1, :], s=1, color="black")
+ax.scatter(x[0, :], x[1, :], s=0.2, color="black")
+
